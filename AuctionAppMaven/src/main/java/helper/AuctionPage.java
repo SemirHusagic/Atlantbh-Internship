c74e5c95defe6122cb690016d5a8431f7e06509e
Semir <semir@cable-77-221-4-137.dynamic.telemach.ba>
2020-01-14 13:50:18 +0100

    window

--- AuctionAppMaven/src/main/java/helper/AuctionPage.java
+++ AuctionAppMaven/src/main/java/helper/AuctionPage.java
@@ -2,6 +2,7 @@ package helper;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 
 public class AuctionPage {
@@ -14,9 +15,12 @@ public class AuctionPage {
 	
 	public void OpenPage() {
 		
+		int width = 1440;
+		int height = 800;
+		Dimension dimension = new Dimension(width, height);
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 		driver.get("https://polar-lake-39918.herokuapp.com/home");
-		driver.manage().window().maximize();
+		driver.manage().window().setSize(dimension);
 	}
 	
 	public void ClosePage() {
